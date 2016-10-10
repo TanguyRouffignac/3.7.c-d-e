5c27d93250c13b7d5464ffa02dd6536a64901e0a
Tanguy <trouffignac@enst.fr>
2016-10-10 14:57:08 +0200

    Getting the number of players in a team from the URL of its page.

--- main.py
+++ main.py
@@ -2,3 +2,8 @@ __author__ = 'Tanguy ROUFFIGNAC'
 
 import mechanize
 from bs4 import BeautifulSoup
+
+br = mechanize.Browser()
+br.open('http://www.echecs.asso.fr/ListeJoueurs.aspx?Action=EQUIPE&Equipe=2792')
+soup = BeautifulSoup(br.response().read(), 'lxml')
+print len(soup("tr")) - 2
