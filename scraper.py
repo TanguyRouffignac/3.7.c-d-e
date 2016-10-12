35950e102736009c1f1f5106df482f11dfd5eea3
Tanguy <trouffignac@enst.fr>
2016-10-12 19:39:38 +0200

    Keeping only the interesting teams for IDF league (other leagues to follow later)

--- scraper.py
+++ scraper.py
@@ -4,6 +4,9 @@ from player import *
 from team import *
 import re
 
+leagues = []
+leagues.append(["Ligue de l'Ile de France",["Essonne 1", "Hauts de Seine 1", "Paris 1", "Seine et Marne 1", "Seine Saint Denis 1", "Val de Marne 1", "Val d'Oise 1", "Yvelines 1"]])
+
 
 class Scraper:
     def __init__(self):
@@ -29,6 +32,13 @@ class Scraper:
         del teams[0]
         del teams[-1]
         for t in teams:
-            if t("td")[1].text == 'Interclubs Adultes' or re.split(' ', t("td")[1].text)[0] == 'Ligue':
+            if t("td")[1].text == 'Interclubs Adultes':
                 team = Team(t("td")[0].text, t("td")[1].text, t("td")[2].text)
                 self.scrape_team(t("td")[0]("a")[0]['href'], team)
+            if re.split(' ', t("td")[1].text)[0] == 'Ligue':
+                for league in leagues:
+                    if league[0] == t("td")[1].text:
+                        for division in league[1]:
+                            if division == t("td")[2].text:
+                                team = Team(t("td")[0].text, t("td")[1].text, t("td")[2].text)
+                                self.scrape_team(t("td")[0]("a")[0]['href'], team)
