fa44a0297ae09b934a472b7ea6293e88e6b65e0e
Tanguy <trouffignac@enst.fr>
2016-10-15 11:14:27 +0200

    Opening a window

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
 from scraper import *
+import gi
+gi.require_version('Gtk', '3.0')
+from gi.repository import Gtk
+
+win = Gtk.Window()
+win.set_title('3.7-c-d-e')
+win.connect("delete-event", Gtk.main_quit)
+win.show_all()
+Gtk.main()
 
 scraper = Scraper()
 results = scraper.search_club('Cre')
