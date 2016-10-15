074b623e5423cc495e0bcdcc087af7ca13e90a53
Tanguy <trouffignac@enst.fr>
2016-10-15 12:12:36 +0200

    Getting every needed input from the GUI

--- mywindow.py
+++ mywindow.py
@@ -22,6 +22,20 @@ class MyWindow(Gtk.Window):
 
     def on_button_clicked(self, widget):
         results = self.scraper.search_club(self.entry.get_text())
-        self.scraper.scrape_club(results[3][3])
+        self.remove(self.box)
+        self.box = Gtk.Box(orientation=Gtk.Orientation.VERTICAL)
+        for r in results:
+            b = Gtk.Button(label = r[2])
+            b.connect("clicked", self.on_club_clicked)
+            b.href = r[3]
+            self.box.pack_start(b, True, True, 0)
+        self.add(self.box)
+        self.show_all()
+
+    def on_club_clicked(self, widget):
+        self.remove(self.box)
+        while Gtk.events_pending():
+            Gtk.main_iteration_do(False)
+        self.scraper.scrape_club(widget.href)
         for t in self.scraper.teams:
             print t
